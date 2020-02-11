813022a91219f84de56cbb3c465b6cbd0ec707cc
Sam Noh <samnoh21@outlook.com>
2020-02-11 21:52:26 +1300

    feat(main): add func

--- main.go
+++ main.go
@@ -32,10 +32,16 @@ func addAllNumbers(numbers ...int) int {
 }
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 1; koreanAge < 18 {
+	if koreanAge := age + 1; koreanAge < 18 { // variable expression
 		return false
 	}
 	return true
+}
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
 }
 
 // entry point
@@ -50,4 +56,34 @@ func main() {
 	fmt.Println(addAllNumbers(1, 2, 3, 4, 5))
 
 	fmt.Println(canIDrink(17))
+
+	// pointers
+	a := 2
+	b := a
+	a = 10
+	fmt.Println(a, b, &a, &b) // 10 2 0xc0000180e8 0xc0000180f0
+
+	c := 2
+	d := &c
+	c = 5
+	*d = 20
+	fmt.Println(c, &c, d, &d, *d) // 5 0xc000018108 0xc000018108 0xc00000e030 5
+
+	// arrays
+	names := [5]string{"h", "e", "l", "l"}
+	names[4] = "o"
+	// names[5] = "b" // erorr; out of bound
+
+	// slices
+	otherNames := []string{"h", "e", "l", "l"}
+	otherNames = append(otherNames, "o")
+
+	me := map[string]string{"name": "sam", "age": "18"}
+	for key, value := range me {
+		fmt.Println(key, value) // name same; age 18
+	}
+
+	favFood := []string{"pizza", "chicken"}
+	sam := person{name: "sam", age: 18, favFood: favFood}
+	fmt.Println(sam)
 }
