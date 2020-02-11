aaebb2482b1609b790509a15d2be3ebc797852fd
Sam Noh <samnoh21@outlook.com>
2020-02-11 21:20:05 +1300

    feat(main): add func

--- main.go
+++ main.go
@@ -22,6 +22,22 @@ func repeatWords(words ...string) {
 	fmt.Println(words)
 }
 
+func addAllNumbers(numbers ...int) int {
+	total := 0
+
+	for _, number := range numbers { // index, number
+		total += number
+	}
+	return total
+}
+
+func canIDrink(age int) bool {
+	if koreanAge := age + 1; koreanAge < 18 {
+		return false
+	}
+	return true
+}
+
 // entry point
 func main() {
 	fmt.Println(multiply(2, 6))
@@ -30,4 +46,8 @@ func main() {
 	fmt.Println(length, uppercase)
 
 	repeatWords("hello", "world", "bye", "world")
+
+	fmt.Println(addAllNumbers(1, 2, 3, 4, 5))
+
+	fmt.Println(canIDrink(17))
 }
