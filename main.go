158b919e8610ca1ed898272b1c8fa5ccdfd250b9
Sam Noh <samnoh21@outlook.com>
2020-02-16 16:04:46 +1300

    feat(main): add hitUrl

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 )
 
-func hitUrl(url string) {
+var (
+	errRequestFailed = errors.New("Request failed")
+)
 
+func hitUrl(url string) error {
+	fmt.Println("Checking:", url)
+	res, err := http.Get(url)
+	if err != nil || res.StatusCode >= 400 {
+		fmt.Println(res.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -19,9 +32,16 @@ func main() {
 		"https://www.facebook.com/",
 		"https://www.instagram.com/",
 	}
-
 	for _, url := range urls {
-		fmt.Println(url)
+		result := "OK"
+		err := hitUrl(url)
+
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
-
